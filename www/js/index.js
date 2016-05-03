593d70c2e2187b35ba5646006aba21c57c733e19
pixelbof <bradley@pixelbof.co.uk>
2016-05-03 16:43:04 +0100

    fixed toolbar and draw layout

--- www/js/index.js
+++ www/js/index.js
@@ -38,21 +38,6 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         //device ready
-        //app.notifications("Welcome to castle club radio", "Live from Ayia Napa!", true, true);
-        $( document ).on( "pagecreate", "#demo-page", function() {
-            $( document ).on( "swipeleft", "#demo-page", function( e ) {
-                // We check if there is no open panel on the page because otherwise
-                // a swipe to close the left panel would also open the right panel (and v.v.).
-                // We do this by checking the data that the framework stores on the page element (panel: open).
-                if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
-                    if ( e.type === "swiperight" ) {
-                        $( "#left-panel" ).panel( "open" );
-                    } else if (e.type === "swipeleft") {
-                        $( "#left-panel" ).panel( "close" );
-                    }
-                }
-            });
-        });
 },
     notifications: function(message, title, autoCancel, ongoing) {
         window.plugin.notification.local.add({
