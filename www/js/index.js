00fde467ee32123918e81cd2b3b8eaa382219bc5
pixelbof <bradley@pixelbof.co.uk>
2016-05-03 15:02:16 +0100

    error fixing

--- www/js/index.js
+++ www/js/index.js
@@ -39,22 +39,21 @@ var app = {
     receivedEvent: function(id) {
         //device ready
         //app.notifications("Welcome to castle club radio", "Live from Ayia Napa!", true, true);
-
         $( document ).on( "pagecreate", "#demo-page", function() {
-            $( document ).on( "swipeleft", "#demo-page", function( e ) {
-                // We check if there is no open panel on the page because otherwise
-                // a swipe to close the left panel would also open the right panel (and v.v.).
-                // We do this by checking the data that the framework stores on the page element (panel: open).
-                if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
-                    if ( e.type === "swiperight" ) {
-                        $( "#left-panel" ).panel( "open" );
-                    } else if (e.type === "swipeleft") {
+            $( document ).on( "swipeleft", "#demo-page", function( e ) {
+                // We check if there is no open panel on the page because otherwise
+                // a swipe to close the left panel would also open the right panel (and v.v.).
+                // We do this by checking the data that the framework stores on the page element (panel: open).
+                if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
+                    if ( e.type === "swiperight" ) {
+                        $( "#left-panel" ).panel( "open" );
+                    } else if (e.type === "swipeleft") {
                         $( "#left-panel" ).panel( "close" );
                     }
-                }
-            });
+                }
+            });
         });
-    },
+},
     notifications: function(message, title, autoCancel, ongoing) {
         window.plugin.notification.local.add({
             id:         "CastleClubRadio",  // A unique id of the notifiction
