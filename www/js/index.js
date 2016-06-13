f7662d55ca7649713800254694ee268c3f9bc3d3
pixelbof <bradley@pixelbof.co.uk>
2016-06-13 23:35:17 +0100

    json data

--- www/js/index.js
+++ www/js/index.js
@@ -81,11 +81,16 @@ $(document).ready(function() {
         }
 
         $("#leftpanel").panel("close");
-        $(".ui-content #page-load").html("Loading...").load('http://castleclubcms.pixelbof.co.uk/api/cms2app/'+pageID, function(data) {
-            $.each(data, function(i, item) {
-                $(".ui-content #page-load").html(data[i].pageHtml);
+
+        var apiCall = 'http://castleclubcms.pixelbof.co.uk/api/cms2app/'+pageID;
+          $.getJSON( apiCall )
+            .done(function( data ) {
+              $.each( data, function( i, item ) {
+                $( ".ui-content #page-load" ).html( item.pageHtml );
+              });
             });
-        });
+
+
         $(".header h1.main-title").html(title);
     });
 
